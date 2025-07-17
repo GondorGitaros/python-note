bb67290a1679b0dde40cd805ac2550aeba7a8593
Csaszar Daniel <csaszardaniel81@gmail.com>
2025-07-17 22:46:08 +0200

    end for today

--- main.py
+++ main.py
@@ -14,27 +14,37 @@ font = pygame.font.SysFont("Times New Roman", 30)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-
             running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            elif event.key == pygame.K_BACKSPACE:
-                if text[line] != "":
-                    text[line] = text[line][:-1]
-                else:
+            if event.key == pygame.K_BACKSPACE:
+                if line == 0 and text[0] == "":
                     print("Nothing to delete")
+                    print(text)
+                elif text[line] == "" and text != "":
+                    text.pop()
+                    line-=1
+                if text[0] != "": 
+                    text[line] = text[line][:-1]
+                    print(text)
+
+            elif event.key == pygame.K_RETURN:
+                text.append("")
+                line+=1
             else:
                 try:
                     text[line] += (event.unicode)
+                    print(text)
                 except:
                     print("Not a key")
 
     screen.fill("grey7")
 
-    text_surface = font.render(text[line], False, (255,255,255))
-    screen.blit(text_surface, (3,3))
+    for i in range(len(text)):
+        text_surface = font.render(text[i], False, (255,255,255))
+        screen.blit(text_surface, (3, i * 30))
+
+
 
     pygame.display.flip()
     clock.tick(180)
